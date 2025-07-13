5b602a422b46d49a5157db2b05144a6aed87cb25
”olek11” <>
2025-07-13 17:12:00 +0700

    lagi perubahan

--- api/index.php
+++ api/index.php
@@ -1,20 +1,8 @@
 <?php
-error_log("Application started at " . date('Y-m-d H:i:s'));
-require __DIR__ . '/index.php'; // Arahkan ke file Laravel jika ada
-?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/build/assets/app-BrkVY816.css">
-    <title>Test</title>
-</head>
-
-<body>
-    <h1 style="color: red;">Test Page</h1>
-    <script src="/build/assets/app-DaBYqt0m.js"></script>
-</body>
-
-</html>
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
