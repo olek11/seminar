96bc53e9ac93de92f86164e7342ac4aecec658b2
”olek11” <>
2025-07-13 16:56:04 +0700

    perbaikan

--- api/index.php
+++ api/index.php
@@ -1,2 +1,20 @@
 <?php
-require __DIR__ . '/../public/index.php';
+error_log("Application started at " . date('Y-m-d H:i:s'));
+require __DIR__ . '/index.php'; // Arahkan ke file Laravel jika ada
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/build/assets/app-BrkVY816.css">
+    <title>Test</title>
+</head>
+
+<body>
+    <h1 style="color: red;">Test Page</h1>
+    <script src="/build/assets/app-DaBYqt0m.js"></script>
+</body>
+
+</html>
